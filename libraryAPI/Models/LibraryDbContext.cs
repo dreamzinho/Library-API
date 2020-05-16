5c15f4bfc0f1276706eebdd94de8c591b7db773f
Piotr Jasiński <jasinski.life@gmail.com>
2020-05-16 11:27:59 +0200

    ADD init project CRUD for books

---
+++ libraryAPI/Models/LibraryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryAPI.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}

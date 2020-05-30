039f9384efcfe3abc726ebf029e3a79eb08b37ef
Michał Zaorski <michal.zaorski98@gmail.com>
2020-05-30 18:02:30 +0200

    more seaded data

--- libraryAPI/Entities/DataSeed.cs
+++ libraryAPI/Entities/DataSeed.cs
@@ -40,7 +40,7 @@ namespace libraryAPI.Entities
                 }
             }
         }
-        public static void Seed3Books(LibraryDbContext _context)
+        public static void SeedBooks(LibraryDbContext _context)
         {
             if(!_context.Books.Any(b => b.Title == "Dzieci Z Bulerbyn"))
             {
@@ -79,6 +79,139 @@ namespace libraryAPI.Entities
                     BookAuthors = null
                 };
                 _context.Books.Add(book3);
+            }
+
+            _context.SaveChanges();
+
+        }
+
+        public static void SeedAuthors(LibraryDbContext _context)
+        {
+            if (!_context.Authors.Any(a => a.Name == "Adam" && a.Surname == "Mickiewicz"))
+            {
+                var author1 = new Author
+                {
+                    Name = "Adam",
+                    Surname = "Mickiewicz",
+                    BookAuthors = null
+                };
+                _context.Authors.Add(author1);
+            }
+
+            if (!_context.Authors.Any(a => a.Name == "Bolesław" && a.Surname == "Prus"))
+            {
+                var author2 = new Author
+                {
+                    Name = "Bolesław",
+                    Surname = "Prus",
+                    BookAuthors = null
+                };
+                _context.Authors.Add(author2);
+            }
+
+            if (!_context.Authors.Any(a => a.Name == "Juliusz" && a.Surname == "Słowacki"))
+            {
+                var author3 = new Author
+                {
+                    Name = "Juliusz",
+                    Surname = "Słowacki",
+                    BookAuthors = null
+                };
+                _context.Authors.Add(author3);
+            }
+
+            _context.SaveChanges();
+
+        }
+        public static void SeedBookAuthor(LibraryDbContext _context)
+        {
+
+            var author1 = _context.Authors.Where(a => a.Name == "Adam" && a.Surname == "Mickiewicz").FirstOrDefault();
+            var book1 = _context.Books.Where(a => a.Title == "Metro 2033").FirstOrDefault();
+            var author2 = _context.Authors.Where(a => a.Name == "Bolesław" && a.Surname == "Prus").FirstOrDefault();
+            var book2 = _context.Books.Where(a => a.Title == "Dzieci Z Bulerbyn").FirstOrDefault();
+            var author3 = _context.Authors.Where(a => a.Name == "Juliusz" && a.Surname == "Słowacki").FirstOrDefault();
+            var book3 = _context.Books.Where(a => a.Title == "O psie, który jeździł kolejom").FirstOrDefault();
+
+            if (!_context.BookAuthor.Any(ba => ba.BookId == book1.Id && ba.AuthorId == author1.Id))
+            {
+                var ba1 = new BookAuthor
+                {
+                    AuthorId = author1.Id,
+                    BookId = book1.Id,
+                    Author = author1,
+                    Book = book1
+                };
+                
+                _context.BookAuthor.Add(ba1);
+                author1.BookAuthors.Add(ba1);
+                book1.BookAuthors.Add(ba1);
+            }
+
+            if (!_context.BookAuthor.Any(ba => ba.BookId == book2.Id && ba.AuthorId == author1.Id) &&
+                !_context.BookAuthor.Any(ba => ba.BookId == book2.Id && ba.AuthorId == author2.Id))
+            {
+                var ba1 = new BookAuthor
+                {
+                    AuthorId = author1.Id,
+                    BookId = book2.Id,
+                    Author = author1,
+                    Book = book2
+                };
+
+                var ba2 = new BookAuthor
+                {
+                    AuthorId = author2.Id,
+                    BookId = book2.Id,
+                    Author = author2,
+                    Book = book2
+                };
+
+                _context.BookAuthor.Add(ba1);
+                author1.BookAuthors.Add(ba1);
+                book2.BookAuthors.Add(ba1);
+
+                _context.BookAuthor.Add(ba2);
+                author2.BookAuthors.Add(ba2);
+            }
+
+            if (!_context.BookAuthor.Any(ba => ba.BookId == book3.Id && ba.AuthorId == author1.Id) &&
+                !_context.BookAuthor.Any(ba => ba.BookId == book3.Id && ba.AuthorId == author2.Id) &&
+                !_context.BookAuthor.Any(ba => ba.BookId == book3.Id && ba.AuthorId == author3.Id))
+            {
+                var ba1 = new BookAuthor
+                {
+                    AuthorId = author1.Id,
+                    BookId = book3.Id,
+                    Author = author1,
+                    Book = book3
+                };
+
+                var ba2 = new BookAuthor
+                {
+                    AuthorId = author2.Id,
+                    BookId = book3.Id,
+                    Author = author2,
+                    Book = book3
+                };
+
+                var ba3 = new BookAuthor
+                {
+                    AuthorId = author3.Id,
+                    BookId = book3.Id,
+                    Author = author3,
+                    Book = book3
+                };
+
+                _context.BookAuthor.Add(ba1);
+                author1.BookAuthors.Add(ba1);
+
+                _context.BookAuthor.Add(ba2);
+                author2.BookAuthors.Add(ba2);
+
+                _context.BookAuthor.Add(ba3);
+                author3.BookAuthors.Add(ba3);
+                book3.BookAuthors.Add(ba3);
             }
 
             _context.SaveChanges();
